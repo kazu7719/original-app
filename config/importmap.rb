e8fc062b08d8f04a89b7c7ccfcd63f1c511a5fc1
kazu7719 <kazuki.eripon7719@icloud.com>
2025-04-16 20:12:45 +0900

    修正

--- config/importmap.rb
+++ config/importmap.rb
@@ -12,3 +12,5 @@ pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/flatpickr.js"
 # FullCalendar 関連（Core + dayGrid）
 pin "@fullcalendar/core", to: "https://cdn.skypack.dev/@fullcalendar/core@6.1.8"
 pin "@fullcalendar/daygrid", to: "https://cdn.skypack.dev/@fullcalendar/daygrid@6.1.8"
+
+pin "controllers/flatpickr_controller", to: "controllers/flatpickr_controller.js"

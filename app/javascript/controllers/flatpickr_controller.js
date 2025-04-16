a8992756c56d68d7d04d919eb29917fc51b7ffdb
kazu7719 <kazuki.eripon7719@icloud.com>
2025-04-16 20:32:09 +0900

    修正

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,6 +3,7 @@ import flatpickr from "flatpickr"
 
 export default class extends Controller {
   connect() {
+    console.log("flatpickr connected") 
     flatpickr(this.element, {
       enableTime: true,
       dateFormat: "Y-m-d H:i",
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 # Stimulus 関連
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "https://ga.jspm.io/npm:@hotwired/stimulus-loading@1.0.0/dist/stimulus-loading.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin_all_from "app/javascript/controllers", under: "controllers" # ✅ controllers ディレクトリの Stimulus コントローラーを一括読み込み
 
 # flatpickr
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/flatpickr.js"
@@ -13,4 +13,3 @@ pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/flatpickr.js"
 pin "@fullcalendar/core", to: "https://cdn.skypack.dev/@fullcalendar/core@6.1.8"
 pin "@fullcalendar/daygrid", to: "https://cdn.skypack.dev/@fullcalendar/daygrid@6.1.8"
 
-pin "controllers/flatpickr_controller", to: "controllers/flatpickr_controller.js"
